94a0c4d732a28e02d63a62663d6809ce12de5983
jmhans <jmhans@hotmail.com>
2019-01-07 17:46:20 -0500

    fixed angular/http call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
     BrowserModule,
     AppRoutingModule, 
     FormsModule, 
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
