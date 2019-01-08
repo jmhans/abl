eb2e6bb6ad466b4905e461d0efb64f741d46d895
jmhans <jmhans@hotmail.com>
2019-01-08 12:54:39 -0500

    barely a change

--- src/app/players/player-list/player-list.component.ts
+++ src/app/players/player-list/player-list.component.ts
@@ -1,16 +1,7 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  Player
-} from '../player';
-import {
-  PlayerService
-} from '../player.service';
-import {
-  PlayerDetailsComponent
-} from '../player-details/player-details.component';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+import { PlayerService } from '../player.service';
+import { PlayerDetailsComponent } from '../player-details/player-details.component';
 
 @Component({
   selector: 'player-list',
@@ -24,13 +15,21 @@ export class PlayerListComponent implements OnInit {
   players: Player[]
   selectedPlayer: Player
 
-  constructor(private playerService: PlayerService) {}
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
-    this.playerService
+     this.playerService
       .getPlayers()
       .then((players: Player[]) => {
-        this.players = players
+        this.players = players.map((player) => {
+          if (!player.phone) {
+            player.phone = {
+              mobile: '',
+              work: ''
+            }
+          }
+          return player;
+        });
       });
   }
 
