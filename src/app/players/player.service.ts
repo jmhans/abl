9a6e777cbfb7b38b551e74a0cae07fe3a49260b4
jmhans <jmhans@hotmail.com>
2019-01-08 12:39:30 -0500

    something little

--- src/app/players/player.service.ts
+++ src/app/players/player.service.ts
@@ -1,53 +1,59 @@
-import { Injectable } from '@angular/core';
-import { Player } from './player';
-import { Http, Response } from '@angular/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Player
+} from './player';
+import {
+  Http,
+  Response
+} from '@angular/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PlayerService {
-  private playersUrl = '/api/players'; 
-  constructor(private http: Http) { }
-  
-  getPlayers(): Promise<void |Player[] > {
+  private playersUrl = '/api/players';
+
+  constructor(private http: Http) {}
+
+  getPlayers(): Promise < void | Player[] > {
     return this.http.get(this.playersUrl)
-    .toPromise()
-    .then(response => response.json() as Player[])
-    .catch(this.handleError);
+      .toPromise()
+      .then(response => response.json() as Player[])
+      .catch(this.handleError);
   }
-  
+
   // post("/api/players")
-    createPlayer(newPlayer: Player): Promise<void | Player> {
-      return this.http.post(this.playersUrl, newPlayer)
-                 .toPromise()
-                 .then(response => response.json() as Player)
-                 .catch(this.handleError);
-    }
+  createPlayer(newPlayer: Player): Promise < void | Player > {
+    return this.http.post(this.playersUrl, newPlayer)
+      .toPromise()
+      .then(response => response.json() as Player)
+      .catch(this.handleError);
+  }
 
-    // get("/api/Players/:id") endpoint not used by Angular app
+  // get("/api/Players/:id") endpoint not used by Angular app
 
-    // delete("/api/players/:id")
-    deletePlayer(delPlayerId: String): Promise<void | String> {
-      return this.http.delete(this.playersUrl + '/' + delPlayerId)
-                 .toPromise()
-                 .then(response => response.json() as String)
-                 .catch(this.handleError);
-    }
+  // delete("/api/players/:id")
+  deletePlayer(delPlayerId: String): Promise < void | String > {
+    return this.http.delete(this.playersUrl + '/' + delPlayerId)
+      .toPromise()
+      .then(response => response.json() as String)
+      .catch(this.handleError);
+  }
 
-    // put("/api/players/:id")
-    updatePlayer(putPlayer: Player): Promise<void | Player> {
-      var putUrl = this.playersUrl + '/' + putPlayer._id;
-      return this.http.put(putUrl, putPlayer)
-                 .toPromise()
-                 .then(response => response.json() as Player)
-                 .catch(this.handleError);
-    }
+  // put("/api/players/:id")
+  updatePlayer(putPlayer: Player): Promise < void | Player > {
+    var putUrl = this.playersUrl + '/' + putPlayer._id;
+    return this.http.put(putUrl, putPlayer)
+      .toPromise()
+      .then(response => response.json() as Player)
+      .catch(this.handleError);
+  }
 
-    private handleError (error: any) {
-      let errMsg = (error.message) ? error.message :
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-      console.error(errMsg); // log to console instead
-    }
-  
-  
+    console.error(errMsg); // log to console instead
+  }
+
+
 }
