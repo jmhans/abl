ce0c3558d756189032987e158590723f0a7bd26c
jmhans <jmhans@hotmail.com>
2019-01-09 13:11:46 -0500

    new version of updateOne

--- server.js
+++ server.js
@@ -156,9 +156,9 @@ app.put("/api/players/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PLAYERS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    console.log("ERROR:" + err);
-    console.log("DOC:" + doc);
+  db.collection(PLAYERS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, {$set : updateDoc}, {upsert : true}, function(err, doc) {
+    console.log(err);
+    console.log(doc);
     
     if (err) {
       handleError(res, err.message, "Failed to update contact");
