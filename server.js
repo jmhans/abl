0dd79d1ade358bda359f578e438ecbeb8adff1b9
jmhans <jmhans@hotmail.com>
2019-01-10 18:43:18 -0500

    trying with mongoose

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 var mongoUtil = require("./models/mongoUtil");
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
+
 
 var CONTACTS_COLLECTION = "contacts";
 var PLAYERS_COLLECTION = "players";
@@ -17,24 +20,36 @@ app.use(express.static(distDir));
 //app.use(require('./routes/routes'));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+var db = mongoose.connection;
 
-// Connect to the database before starting the application server.
-mongoUtil.connectToServer( function (err) {
-  if (err) {
-    console.log("I'm logging this one now." + err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  //db = mongoUtil.getDb(); //client.db();
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
   console.log("Database connection ready");
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+
 })
+
+
+// Connect to the database before starting the application server.
+// mongoUtil.connectToServer( function (err) {
+//   if (err) {
+//     console.log("I'm logging this one now." + err);
+//     process.exit(1);
+//   }
+
+//   // Save database object from the callback for reuse.
+//   //db = mongoUtil.getDb(); //client.db();
+//   console.log("Database connection ready");
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// })
 
 
 // mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
