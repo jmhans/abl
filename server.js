5e61b73785ef76dfc92828a6eed679b768bdbdd3
jmhans <jmhans@hotmail.com>
2019-01-17 10:13:52 -0500

    Fixing base controller

--- server.js
+++ server.js
@@ -128,5 +128,5 @@ app.use('/api', api);
 
 const OwnersController = require('./controllers/owners.controller');
 
-app.use('/api/owners', new OwnersController().route());
+app.use('/api/owners', new OwnersController().route);
 //Inconsequential change.
