2db2908dfb9d26e383981da0b71d10bf1e53ad0c
jmhans <jmhans@hotmail.com>
2019-01-17 14:10:01 -0500

    minor change

--- routes/api.route.js
+++ routes/api.route.js
@@ -22,4 +22,4 @@ router.get('/players/:id', players_controller.player_getOne);
 router.put('/players/:id', players_controller.player_update);
 router.delete('/players/:id', players_controller.player_delete);
 
-exports = router;
+module.exports = router;
